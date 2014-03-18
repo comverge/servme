92d2769e68de8b97ad4ad36ad26c76f704791bcd
Ben Lieber <blieber@comverge.com>
2014-03-18 13:21:57 -0600

    Allow for "fuzzy" stubbers
    Can pass in wildcards "*" or a string RegEx "/[1|2]/"
    as parts of paths or params and they will match
    appropriately.
    
    It will use the first one it can find that matches everything,
    so careful...

--- lib/servme/stubber.rb
+++ lib/servme/stubber.rb
@@ -36,12 +36,45 @@ module Servme
     end
 
     def stub_for_request(req)
-      method = req.request_method.downcase.to_sym
-      begin
-        @stubbings[req.path][method][req.params]
-      rescue NoMethodError
-        nil
-      end
+      valid_paths = filter_paths(req.path, @stubbings)
+      valid_methods = filter_methods(req.request_method, valid_paths)
+      valid_params = filter_params(req.params, valid_methods)
+      puts "*** WARNING: multiple stubs match requested route (#{req.path}. Using first. ***" if valid_params.length > 1
+      valid_params.first
+    rescue NoMethodError
+      nil
+    end
+
+    def filter_methods(request, stubs)
+      stubs.map{|path|
+        path.map{|k, v| v if request.downcase.to_sym == k }.compact
+      }.flatten
+    end
+
+    def filter_paths(request, stubs)
+      stubs.map{|path, data|
+        data if path.split("/").each_with_index.all?{|path_part, i|
+          matches?(path_part, request.split("/")[i])
+        }
+      }.compact
+    end
+
+    def filter_params(request, stubs)
+      stubs.map{|stub|
+        stub.map{|stubbed_params, response|
+          response if stubbed_params == request || stubbed_params.detect{|k,v|
+            request.all?{|rpk, rpv| matches?(v, rpv) }
+          }
+        }.compact
+      }.flatten
+    end
+
+    def matches?(val1, val2)
+      val1 = val1.to_s
+      val2 = val2.to_s
+      val1 == val2 || val1 == '*' || (/^\/.*\/$/.match(val1) && Regexp.new(val1) =~ val2)
+    rescue
+      false
     end
 
     def stringify_keys(params)
